f0be567a0458761d9cce920f4ec6b3ffaae8da33
iyanuloluwa-Miracle <iyanudina@gmail.com>
2023-02-09 22:41:30 +0100

    added few changes

--- app.js
+++ app.js
@@ -60,7 +60,8 @@ mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true})
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-  googleId: String
+  googleId: String,
+  secret: String
 })
 
 
@@ -133,12 +134,18 @@ app.get("/register", function(req, res){
 
 
 app.get('/secrets', function(req, res){
-  if (req.isAuthenticated()){
-    res.render("secrets")
-  }else{
-    res.redirect("/login")
-  }
+  User.find({"secrets": {$ne: null}}, function(err, foundUser){
+    if(err){
+      console.log(err)
+    } else{
+      if(foundUser) {
+        res.render("secrets", {usersWithSecrets : foundUser})
+      }
+    }
+  })
+  
 })
+
 app.get('/logout', function(req, res, next) {
   req.logout(function(err) {
     if (err) { 
@@ -253,6 +260,36 @@ app.post("/login", function(req, res){
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
+
+
+//submit route
+
+app.get('/submit', function(req, res){
+  if (req.isAuthenticated()){
+    res.render("submit")
+  }else{
+    res.redirect("/login")
+  }
+})
+app.post("/submit", function(req,res){
+  const submittedSecret = req.body.secret
+
+  User.findById(req.user.id, function(err, foundUser){
+    if(err){
+      console.log(err)
+    }else{
+      if(foundUser){
+        foundUser.secret = submittedSecret
+        foundUser.save(function(){
+          res.redirect("/secrets")
+        })
+
+      }
+    }
+
+  })
+})
+
 
 
 app.listen(8001, () => {
